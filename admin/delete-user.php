d4e2f685e25a6e1b6c8552cc635a97b8b5ef81b1
Sajan Kc <sajan1996kc@gmail.com>
2021-03-24 15:19:19 +0545

    delete user feature added

---
+++ admin/delete-user.php
@@ -0,0 +1,28 @@
+<?php
+require_once("../includes/dbcon.rec.php");
+// require_once "includes/auth.php";
+?>
+<html>
+
+<head>
+     <?php include_once("../includes/head.inc.php"); ?>
+</head>
+
+<body>
+     <?php
+     if (isset($_GET['id']) && !empty($_GET['id'])) {
+          $id = $_GET['id'];
+          $query = "DELETE FROM user WHERE `uid`=:id";
+          $stmt = $pdo->prepare($query);
+          $stmt->bindParam(':id', $id);
+          $stmt->execute();
+          echo '<h2>Deletinging user...</h2>';
+          header('Refresh: 1; URL = dashboard.php');
+     } else {
+          echo '<h2>No record specified to delete.</h2>';
+          header('Refresh: 1; URL = dashboard.php');
+     }
+     ?>
+</body>
+
+</html>
