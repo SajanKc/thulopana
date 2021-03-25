142ef842a341b5c8c1b067714416113f8ddeb4af
Sajan Kc <sajan1996kc@gmail.com>
2021-03-25 19:00:31 +0545

    path to home page fixed

--- admin/includes/navbar.inc.php
+++ admin/includes/navbar.inc.php
@@ -8,12 +8,13 @@ if (isset($_SESSION['logged_user'])) {
      <ul class="navbar">
           <li class="logo">
                <!-- <img src="images/brand-logo.png" alt="logo here" /> -->
-               <h2><a style="font-size:4rem; color:white; text-shadow: 2px 2px 8px #FF0000;" href="index.php"> ThuloPana </a></h2>
+               <h2><a style="font-size:4rem; color:white; text-shadow: 2px 2px 8px #FF0000;" href="../index.php"> ThuloPana </a></h2>
           </li>
           <li>
                <?php if ($logged_user === 'guest' || $logged_user === "") {
                     echo '<a class="btn-all" href="../login.php"> Login </a>';
                } else {
+                    echo '<h2 style="display:inline; background-color:red; padding:3.5px 5px; margin-right:5px; font-size:22px;">Welcome ' . $logged_user . '</h2>';
                     echo '<a class="btn-all" href="../logout.php"> Logout </a>';
                }
                ?>
