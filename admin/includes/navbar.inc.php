756b59186dad168c5117f37c1958765ee878642b
Sajan Kc <sajan1996kc@gmail.com>
2021-03-28 15:26:52 +0545

    path set to default

--- admin/includes/navbar.inc.php
+++ admin/includes/navbar.inc.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['logged_user'])) {
           </li>
           <li>
                <?php if ($logged_user === 'guest' || $logged_user === "") {
-                    echo '<a class="btn-all" href="login.php"> Login </a>';
+                    echo '<a class="btn-all" href="../login.php"> Login </a>';
                } else {
                     echo '<h2 style="display:inline; background-color:red; padding:3.5px 5px; margin-right:5px; font-size:22px;">Welcome ' ?>
                     <?php if ($role === 'admin') {
@@ -22,7 +22,7 @@ if (isset($_SESSION['logged_user'])) {
                     }
                     ?>
                <?php echo '</h2>';
-                    echo '<a class="btn-all" href="logout.php"> Logout </a>';
+                    echo '<a class="btn-all" href="../logout.php"> Logout </a>';
                }
                ?>
           </li>
